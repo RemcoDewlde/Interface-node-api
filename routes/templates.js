1c0bfde95f551c47b8dd5ac9b7d8dbf1367ebb3c
remco <dewilderemco13@gmail.com>
2020-08-01 12:14:06 +0200

    cleaned the template route

--- routes/templates.js
+++ routes/templates.js
@@ -4,51 +4,43 @@ const router = express.Router();
 let Template = require('../models/template.js');
 
 router.get('', function (req, res) {
-    Template.find().exec().then((data) => {
-        try {
-            res.json(data);
-        } catch (e) {
-            throw e
-        }
-    })
+    Template.find()
+        .then((data) => res.status(200).json(data))
+        .catch((err) => res.status(400).json(err))
 });
 
-router.post('', function (req, res, next) {
-    let template = new Template(
-        req.body
-    );
-    template.save();
-    res.json({ok: true, message: "Template saved successfully"})
+router.post('', function (req, res) {
+    const { Name, ProductPrice, status, marge, sale, salePrice, SellingPoints } = req.body;
+    let template = new Template({
+        Name,
+        ProductPrice,
+        status,
+        marge,
+        sale,
+        salePrice,
+        SellingPoints
+    });
+    template.save()
+        .then((data) => res.status(200).json({ok: true, message: "Template saved successfully"})
+        .catch((err) => res.status(400).json("Request Failed")));
 });
 
-router.get('/:id', function (req, res, next) {
-    Template.findById(req.params.id, function (err, data) {
-        res.json(data);
-    })
+router.get('/:id', (req, res) => {
+    Template.findById(req.params.id)
+        .then((data) => res.status(200).json(data))
+        .catch((err) => res.status(400).json(err))
 });
 
-router.delete('/:id', function (req, res, next) {
-    Template.findByIdAndDelete({_id: req.params.id}, function (err, result) {
-        if (err) throw err;
-        if (result) {
-            res.json({success: true, message: "Deleted successfully"})
-        }
-    })
+router.delete('/:id',(req, res) => {
+    Template.deleteOne({_id: req.params.id})
+        .then((data) => {res.status(200).json({success: true, message: "Deleted successfully", payload: {data}})})
+        .catch((err) => {res.status(400).json("Request Failed")});
 });
 
-router.post('/search', function (req, res, next) {
-    Template.find({
-        Name: {
-            $regex: req.body.search,
-            $options: 'i'
-        }
-    })
-        .exec((err, result) => {
-            res.json({found: result});
-            if (err) {
-                next(err)
-            }
-        })
+router.post('/search', function (req, res) {
+    Template.find({Name: { $regex: req.body.search, $options: 'i'}})
+        .then((data) => {res.status(200).json({found: data})})
+        .catch((err) => {res.status(400).json("Request Failed")})
 });
 
 
